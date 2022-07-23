e2c49ea72188eeafab2f14a6ac5beb656a009ae9
Nitin Singh <nsinghboss02@gmail.com>
2022-07-23 11:42:51 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -17,7 +18,9 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User",userSchema);
@@ -26,7 +29,7 @@ const User = new mongoose.model("User",userSchema);
 app.post("/register",function(req,res){
     const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password) // converted into hash codes
     
  })
 
@@ -45,7 +48,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err,foundUser){
         if(err){
             console.log(err);
